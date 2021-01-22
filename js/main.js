2946da3af7e1910b42670eb11fdea1c53bd4fecf
Ivan Shulga <shg.ivan97@gmail.com>
2021-01-22 04:28:37 +0300

    categories is over

--- js/main.js
+++ js/main.js
@@ -3,25 +3,42 @@ function navMobile() {
     $('#navigation').toggleClass('navigationActive');
 }
 
-$('.instagram-wrapper').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    dots: false,
-    responsive:{
-        0:{
-            items:1,
-            dots: true
-        },
-        600:{
-            items:3,
-            dots: true
-        },
-        1000:{
-            items:4
-        }
-    }
-})
+$(function(){
+    $.mask.definitions['9'] = '';
+    $.mask.definitions['d'] = '[0-9]';
+    $(".number_mask").mask("+971dd-ddd-dddd", {placeholder: " "});
+});
+
+$(window).on('load', function(){
+          $.instagramFeed({
+            'username': 'da_hookah',
+            'container': "#instafeed",
+            'display_profile': false,
+            'display_biography': false,
+            'styling': false
+          });
+          $(".instagram_gallery").addClass("owl-carousel owl-theme")
+          $('.instagram-wrapper>.instagram_gallery').owlCarousel({
+            loop:true,
+            margin:0,
+            nav:true,
+            dots: false,
+            responsive:{
+                0:{
+                    items:1,
+                    dots: true
+                },
+                600:{
+                    items:3,
+                    dots: true
+                },
+                1000:{
+                    items:4
+                }
+            }
+        })
+        });
+
 
 var $input = $('#shishasInput'),
     $buffer = $('.input-buffer');
@@ -59,14 +76,14 @@ function minusHours() {
     if((hours.value-1) <= 0) { hours.value = '0'} else {
         hours.value = `${hours.value-1}`
     }
-    $buffer1.text($input.val());
-    $input1.width($buffer.width());
+    $buffer1.text($input1.val());
+    $input1.width($buffer1.width());
 }
 function plusHours() {
     const hours = document.getElementById('hoursInput');
     hours.value = `${parseInt(hours.value, 10)+1}`
-    $buffer1.text($input.val());
-    $input1.width($buffer.width());
+    $buffer1.text($input1.val());
+    $input1.width($buffer1.width());
 }
 
 function inputHours() {
@@ -74,6 +91,6 @@ function inputHours() {
     if((hours.value-1) <= 0) { hours.value = '0'} else {
         hours.value = `${hours.value-1}`
     }
-    $buffer1.text($input.val());
-    $input1.width($buffer.width());
+    $buffer1.text($input1.val());
+    $input1.width($buffer1.width());
 }
